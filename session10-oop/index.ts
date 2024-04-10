7e7cc2ad70f5e29ce121a6e00db0002eccbdb044
trinhquochai <trinhquochai12@gmail.com>
2024-04-10 16:40:24 +0700

    add

---
+++ session10-oop/index.ts
@@ -0,0 +1,105 @@
+/* 
+    các cách tạo đối tượng trong js
+    1. let,const,var_ tên đối tượng ={}
+    2. dùng new Object()
+    3. 
+        tạo hàm tạo constructor
+        function Student (name){
+            this.name=name
+        }
+        let std1= new Student("minh");
+        // đây mới là đi tạo đối  tượng
+    4. 
+        tạo class
+        class Person{
+            constructor(name,age,address){
+                this.name=name;
+                this.age=age;
+                this.address=address
+            }
+        }
+        let std1= new Person("hoa",20,"hà nội");
+        // đây mới là tạo đối tượng std1 thông qua từ khóa new
+        1. đối tượng sẽ gồm 2 thành phần
+         + thuộc tính (property)
+         + phương thức (hành vi method)
+         OOP: gồm 4 tính chất
+         1. tính đóng gói (encapsulation)
+            + che giấu thông tin của đối tượng
+            ==> nhằm giúp cho các truy cập đến tượng được bảo mật hơn 
+         2. tính trừu tượng (abstraction)
+         3. tính kế thừa (inheritance)
+         4. tính đa hình ( polymorphism)
+ */
+class Person{
+   public name1:string // mặc định phạm vi truy cập là public
+   private age1:number 
+   // trong trường hợp phạm vi là private
+   // để truy cập được thì thông qua phương thức
+   // lấy ra: get
+   // set
+    firstName: string
+    lastName:string
+    private address:string
+    constructor(name:string,age:number,firstName:string,lastName:string,address:string){
+        this.name1=name;
+        this.age1=age;
+        this.firstName=firstName;
+        this.lastName=lastName;
+        this.address=address;
+    }
+    // nơi khai báo những mothod
+    // ví dụ khai báo 1 phương thức show thông tin của user
+    showInfo():void{
+        console.log("xin chào",this.firstName+this.lastName);
+    }
+    showName():void{
+        console.log("xin chào",this.name1);
+    }
+    getAge():number{
+       return this.age1;
+    }
+    setAge(age:number){
+        this.age1=age;
+    }
+    // getter
+    get address1(){
+        return this.address
+    }
+    //setter
+    set address1(address:string){
+        this.address=address
+    }
+}
+let std1= new Person("minh thu",20,"minh","thu","hà nội");
+
+// std1: được gọi là 1 instance của class Person
+console.log("std1",std1);
+// gọi phương thức
+std1.showInfo();
+// tính đóng gói : encapsulation
+// access modifier -phạm vi truy cập
+/* 
+    1. public
+    2. private
+    3. proceted liên quan đến kế thừa
+    - khi class con kế thừa class cha thì class con sẽ có
+    các thuộc tính và phương thức của class cha
+    - khi phạm vi truy cập proceted thì class con không truy cập được thuộc tính 
+    của class cha
+ */
+std1.name1="minh hoàng";
+std1.showName();
+// đi set tuổi minh thu
+std1.setAge(22);
+// console.log(std1.getAge());
+// console.log(std1.address1);
+// đi thay đổi địa chỉ thông qua phương thức setter
+std1.address1="sài gòn";
+// hiển thị địa chỉ
+console.log(std1.address1);
+
+
+
+
+
