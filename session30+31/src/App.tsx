ec7b1265f7d973b63b28c9638672eb930c4df649
giabao <thainguyengiabao082105>
2024-05-09 11:33:52 +0700

    add

--- session30+31/src/App.tsx
+++ session30+31/src/App.tsx
@@ -3,11 +3,8 @@ import Exercise01 from './components/BaiTap/Ex01/Exersice01'
 import Notification from './components/BaiTap/Ex02/Notification'
 import Reacts from './components/BaiTap/Ex03/Reacts'
 import Ex04 from './components/BaiTap/Ex04/Ex04'
-import Ex055 from './components/BaiTap/Ex05/Ex05'
-import Ex05Child from './components/BaiTap/Ex05/Ex05Child'
 import Ex06 from './components/BaiTap/Ex06/Ex06'
-import Ex06Child from './components/BaiTap/Ex06/Ex06Child'
-
+import Ex055 from './components/BaiTap/Ex05/Ex05'
 
 export default function App() {
   return (
@@ -17,9 +14,8 @@ export default function App() {
       <Reacts></Reacts>
       <Ex04></Ex04>
       <Ex055></Ex055>
-      <Ex05Child></Ex05Child>
       <Ex06></Ex06>
-      <Ex06Child></Ex06Child>
+
     </div>
   )
 }
